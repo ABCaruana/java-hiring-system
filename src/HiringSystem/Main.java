f72260076abbffb4be8369a897797736c19ad50b
Loaf <acaruanaop@gmail.com>
2022-01-28 15:02:22 +0000

    update main

---
+++ src/HiringSystem/Main.java
@@ -0,0 +1,24 @@
+package HiringSystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("Main Menu - Job Portal");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
