ecb5fbc3ea3b13a25416227aa25674b02d311ebf
Loaf <acaruanaop@gmail.com>
2022-01-28 15:02:40 +0000

    update controllers

--- src/HiringSystem/mainWindowController.java
+++ src/HiringSystem/mainWindowController.java
@@ -1,4 +1,4 @@
-package sample;
+package HiringSystem;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,12 +11,29 @@ import java.io.IOException;
 public class mainWindowController {
     @FXML
     private void staffLogin(){
-//        open loginWindow stage
+        // open loginWindow stage
         System.out.println("opening staff login page");
         try {
             Parent root = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
             Stage stage = new Stage();
             stage.setTitle("Staff login portal");
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void applicantForm(){
+        // open applicant form
+        System.out.println("opening applicant form");
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("ApplicantFormWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Applicant submission form");
             stage.setScene(new Scene(root));
             stage.setResizable(false);
             stage.show();
